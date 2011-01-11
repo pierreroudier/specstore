0a1f89005675658f855349dcfa0ef08a24e31379
Pierre Roudier <pierre.roudier@gmail.com>
2011-01-11 17:29:33 +1300

    Added initializer. Changed also a bit the structure of the object. Still not sure wether I'll use list() or matrix() to store reflectances.

--- R/class_defs.R
+++ R/class_defs.R
@@ -3,15 +3,36 @@
 ##
 
 setClass(
-  Class='spectra',
+  Class='Spectra',
   representation=representation(
     wl='numeric',
-    ref='numeric',
-    id='character'
+    ref='list',
+    id='character',
+    data='data.frame'
   ),
   prototype=prototype(
-    wl=NA,
-    ref=NA,
-    id='NA'
+    wl=0,
+    ref=list(),
+    id="",
+    data=data.frame()
   )
 )
+
+## 
+## Class initializer
+##
+
+setMethod(
+  f='initialize', 
+  signature='Spectra', 
+  definition=function(.Object, wl=0, ref=list(), id="", data=data.frame()){
+    .Object@wl <- wl
+    .Object@ref <- ref
+    .Object@id <- id
+    .Object@data <- data
+    # call inspector to check for horizon level errors
+    validObject(.Object)
+    return(.Object)
+  }
+)
+
