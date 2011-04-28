5da30b28db6dfc73e33a7225ff3269b5caf27d11
Pierre Roudier on mangatainoka <pierre.roudier@gmail.com>
2011-04-28 23:24:46 +1200

    introduced continuum removal methods

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -36,3 +36,51 @@ rnv <- function(x, r){
   }
   res
 }
+
+## Continuum removal
+##
+remove_continuum <- function(obj){
+
+# # using plyr
+#   require(plyr)
+#   require(reshape)
+#   res <- aaply(.data=spectra(obj), .margins=1, .fun=function(x) {
+#     ch.index <- sort(chull(x))
+#     ch <- data.frame(wl=wl(obj)[ch.index], nir=x[ch.index])
+#     ch <- approx(x=ch$wl, y=ch$nir, xout=wl(obj)) 
+#     x - ch$y
+#     })
+
+  # using a for loop
+  sp <- spectra(obj)
+  res <- sp
+
+  for (i in 1:nrow(sp)) {
+    ch.index <- sort(chull(sp[i,]))
+    ch <- data.frame(wl=wl(obj)[ch.index], nir=sp[i, ch.index])
+    ch <- approx(x=ch$wl, y=ch$nir, xout=wl(obj))
+    res[i, ] <- sp[i, ] - ch$y
+  }
+
+  obj@nir <- res
+  obj
+}
+
+## from Raphael
+continuum.removal <- function(obj, wavelength=wl(obj), method="linear"){
+  require(signal)
+
+  spc <- spectra(obj)
+  cr <- spc
+
+  for (i in 1:nrow(spc)){
+    id <- sort(chull(wavelength, spc[i,]))
+    cr[i, ] <- spc[i,] - interp1(wavelength[id], spc[i,id], wavelength, method=method, extrap=TRUE)
+  }
+  obj@nir <- cr
+  obj
+}
+
+## for baseline, see the baseline package
+
+## what about derivatives? See KernSmooth package. (locpoly function)
