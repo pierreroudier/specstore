74d7b19d9714003ed6277db2cbc6b0961a8d995d
Pierre Roudier on mangatainoka <pierre.roudier@gmail.com>
2011-04-28 23:33:03 +1200

    first inplementation of a sdp inheritance for Spectra* objects

---
+++ R/Class-SpatialSpectraDataFrame.R
@@ -0,0 +1,18 @@
+setClass(
+  Class='SpatialSpectraDataFrame',
+  representation=representation(
+    'SpatialPointsDataFrame',
+    'SpectraDataFrame'
+  ),
+  prototype=prototype(
+    bbox = matrix(NA), 
+    proj4string = CRS(as.character(NA)),
+    coords = matrix(NA), 
+    coords.nrs = numeric(0),
+    wl=numeric(),
+    nir=matrix(),
+    id=as.character(NA),
+    units=as.character(NA),
+    data = data.frame()
+  )
+)
