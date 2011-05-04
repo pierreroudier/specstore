342bf2169f24b20e0be7432711e7bd63011eb28b
Pierre Roudier <pierre.roudier@gmail.com>
2011-05-04 17:37:32 +1200

    roxygen doc added

--- R/big_head.R
+++ R/big_head.R
@@ -4,6 +4,24 @@
 ## Not only big.head() gives a limited set of rows,
 ## but it laso gives a limited number of columns.
 ##
+
+#' Return the First or Last Part of an Object
+#' 
+#' Returns the first or last rows of a data frame like head() and tail(),
+#' but also only returns the first and last columns. This has been
+#' implemented to check big data frames.
+#'
+#' @param x a data.frame
+#' @param n a single, positive integer, number of rows for the object to return 
+#' @param s a single, positive integer
+#' @param f a single, positive integer
+#' @examples 
+#' big.head(mtcars)
+#' big.tail(mtcars)
+#' big.tail(mtcars, 10)
+#' big.head(mtcars, 10, 2, 4)
+#' big.head(mtcars, , , 1)
+#'
 big.head <- function(x, n=5, s=5, f=5){
   
   stopifnot(length(s) == 1L)
