85e5ca32f8951ed1850a05aedec0dc096794b24f
Pierre Roudier <pierre.roudier@gmail.com>
2011-05-20 15:35:26 +1200

    first commit

---
+++ R/specstore-package.R
@@ -0,0 +1,19 @@
+#' Stores and eases the manipulation of Vis-NIR spectra and associated data
+#'
+#' \tabular{ll}{
+#' Package: \tab specstore\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1.0\cr
+#' Date: \tab 2011-05-20\cr
+#' License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' 
+#' @name specstore-package
+#' @aliases specstore
+#' @docType package
+#' @title Stores and eases the manipulation of Vis-NIR spectra and associated data
+#' @author Pierre Roudier \email{pierre.roudier@@gmail.com}
+#' @keywords spectroscopy
+roxygen()
