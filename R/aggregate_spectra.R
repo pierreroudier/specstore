f9c89a7f51ec3979653f53ba0b725e04a618ae2f
Pierre Roudier <pierre.roudier@gmail.com>
2011-06-07 14:26:46 +1200

    fixed bug #1 - aggregate_spectra 'evaluation nested too deeply' error

--- R/aggregate_spectra.R
+++ R/aggregate_spectra.R
@@ -1,7 +1,7 @@
-#' Aggregates the spectral information of a Spectra object using 
+#' Aggregates the spectral information of a Spectra object using
 #' an aggregation function chosen by the user.
 #'
-#' Aggregates the spectral information of a Spectra object using 
+#' Aggregates the spectral information of a Spectra object using
 #' an aggregation function chosen by the user.If some data is also
 #' present, it is aggregated using the same function.
 #'
@@ -12,23 +12,28 @@
 #' @export
 #' @author Pierre Roudier \url{pierre.roudier@@gmail.com}
 #' @import plyr
-aggregate_spectra <- function(obj, fun = mean, ...){
 
-  # making up an id name from the aggregation function
-  id <- as.character(substitute(fun))[1]
-  
-  # applying the function to the spectra
-  nir <- aaply(.data=spectra(obj), .margins=2, .fun=fun, ...)
+if (!isGeneric("aggregate_spectra"))
+  setGeneric("aggregate_spectra", function(obj, fun = mean, ...)
+    standardGeneric("aggregate_spectra"))
 
-  res <- Spectra(wl=wl(obj), nir=nir, id=id, units=units(obj))
+setMethod("aggregate_spectra", "Spectra",
+  function(obj, fun = mean, ...){
+    # making up an id name from the aggregation function
+    id <- as.character(substitute(fun))[1]
 
-  # if there is some data
-  if (is(obj, 'SpectraDataFrame')) {
-    data <- aaply(.data=data(obj), .margins=2, .fun=fun, ...)
-    res <- SpectraDataFrame(res, data=data.frame(matrix(data, nrow=1, dimnames=list(id, names(data)))))
+    # applying the function to the spectra
+    nir <- aaply(.data = spectra(obj), .margins = 2, .fun = fun, ...)
+
+    res <- Spectra(wl=wl(obj), nir=nir, id=id, units=units(obj))
+
+    # if there is some data
+    if ("data" %in% slotNames(obj)) {
+#     if (is(obj, 'SpectraDataFrame')) {
+      data <- aaply(.data = data(obj), .margins = 2, .fun = fun, ...)
+      res <- SpectraDataFrame(res, data=data.frame(matrix(data, nrow=1, dimnames=list(id, names(data)))))
+    }
+
+    res
   }
-  
-  res
-}
-
-setMethod("aggregate_spectra", "Spectra", aggregate_spectra)
+)
